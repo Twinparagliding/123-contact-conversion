2d3c3e0b6c4723dae4fc9f74d7963a556e40cd1a
Twinparagliding <44975324+Twinparagliding@users.noreply.github.com>
2018-11-26 10:55:38 +0100

    Update adscode.js

--- adscode.js
+++ adscode.js
@@ -7,3 +7,14 @@
 
   gtag('config', 'AW-878702734');
 </script>
+
+
+<!--bitte ändern sie global sit tag in google tag manager snippet code -->
+
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-P589PFP');</script>
+<!-- End Google Tag Manager -->
