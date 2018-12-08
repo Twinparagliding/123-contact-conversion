9213736bf9f3e3a54b0371fef43e1c9db8df339c
Twinparagliding <44975324+Twinparagliding@users.noreply.github.com>
2018-12-08 21:24:52 +0100

    Update adscode.js

--- adscode.js
+++ adscode.js
@@ -1,20 +1,11 @@
-<!-- Global site tag (gtag.js) - Google Ads: 878702734 -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=AW-878702734"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'AW-878702734');
-</script>
-
-
-<!--bitte ändern sie global site tag in google tag manager snippet code -->
-
-<!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+(function(w,d,s,l,i){w[l]=w[l}}{{];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-P589PFP');</script>
-<!-- End Google Tag Manager -->
+})(window,document,'script','dataLayer','GTM-P589PFP');
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'AW-878702734');
